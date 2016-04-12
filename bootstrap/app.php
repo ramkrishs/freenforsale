a8c08724893c9e02e4b91d6595762f057742b0ba
Ramakrishnan Sathyavageeswaran <ramkrishs@outlook.com>
2016-04-12 00:56:05 -0500

    adding mongodb

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -41,6 +41,9 @@ $app->singleton(
     App\Exceptions\Handler::class
 );
 
+$app->register('Jenssegers\Mongodb\MongodbServiceProvider');
+
+$app->withEloquent();
 /*
 |--------------------------------------------------------------------------
 | Return The Application
